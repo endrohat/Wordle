a9197c5798fe90443c42dfa28e0afff413256b32
indraneel <indraneel.potnis@gmail.com>
2022-01-23 22:12:31 +0530

    Initial Commit

---
+++ Wordle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wordle
+//
+//  Created by indraneel potnis on 23/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by indraneel potnis on 23/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
